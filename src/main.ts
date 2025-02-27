91cd71409bad07c68073f3efe88ae2629dea8e06
Tamta <tamtakopadz16@gmail.com>
2025-02-27 21:58:25 +0400

    Set up API prefix and enable CORS for frontend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors(
+    'http://localhost:4200/'
+  )
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
