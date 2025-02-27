12b7caacdcfb01f92be0e39f05abc07c4da55168
Tamta <tamtakopadz16@gmail.com>
2025-02-27 22:04:06 +0400

    add initial routes

--- src/recipes/recipes.controller.ts
+++ src/recipes/recipes.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('recipes')
 export class RecipesController {
@@ -6,4 +6,20 @@ export class RecipesController {
     async recipes (){
         return 'hellooo'
     }
+
+    @Post()
+    async createRecipe(){
+        return ' recipe created successfully'
+    }
+
+    @Put(':id')
+    async feedAnimal(@Param('id') id: string) {
+      return 'recipe edited successfully';
+    }
+
+    @Delete()
+    async deleteRecipe(@Param('id') id: string) {
+        return 'Recipe deleted successfully';
+    }
+    
 }
