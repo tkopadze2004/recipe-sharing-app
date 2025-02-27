8a27c45e1d061aaaa8b2afa157460269e0cfd734
Tamta <tamtakopadz16@gmail.com>
2025-02-28 01:07:21 +0400

    add mongodb url

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,9 @@ import { RecipeModule } from './recipes/recipes.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/recipes'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/recipes'),
+    MongooseModule.forRoot('mongodb+srv://tata:zqSO19SRzfmzBVtM@cluster0.8jsir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+
     RecipeModule,
   ],
   controllers: [AppController],
