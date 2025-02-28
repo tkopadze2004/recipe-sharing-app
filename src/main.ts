7c3c6f4ff77ee9da1ec78861496e83351be9ece9
Tamta <tamtakopadz16@gmail.com>
2025-02-28 10:44:28 +0400

    Add CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors(
-    'http://localhost:4200/'
-  )
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'https://animal-farm-456c4.web.app',
+      'https://animal-farm-456c4.firebaseapp.com',
+    ],
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
