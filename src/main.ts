38f5711d7c27139b9150dca8b3f7521836b1bf76
Tamta <tamtakopadz16@gmail.com>
2025-02-28 10:57:56 +0400

    add global validation pipe with whitelist and transformation

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   app.enableCors({
-    origin: [
-      'http://localhost:4200',
-      'https://animal-farm-456c4.web.app',
-      'https://animal-farm-456c4.firebaseapp.com',
-    ],
+    origin: ['http://localhost:4200', 'https://recipe-space-6dece.web.app/'],
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
     credentials: true,
